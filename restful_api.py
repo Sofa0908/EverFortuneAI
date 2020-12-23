15f04652b2ef18bafc61456e2a77193cd7b8a8ef
Vincent Yang <vvincent0908tw@gmail.com>
2020-12-23 10:45:52 +0800

    update: second attempt at JWT
    gets {"msg": "Token has been revoked"} when testing tokens for logout/refresh/access
    
    Login works normally, but somehow it seems like the model doesn't recognize the tokens it sent out.

--- restful_api.py
+++ restful_api.py
@@ -1,21 +1,22 @@
 import re
+import pdb
 
 from flask import Flask, jsonify, request, session
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (
+  JWTManager,
+  create_access_token,
+  create_refresh_token,
+  jwt_required,
+  jwt_refresh_token_required,
+  get_jwt_identity,
+  get_raw_jwt
+)
 from flask_marshmallow import Marshmallow
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 from werkzeug.security import check_password_hash, generate_password_hash
 
-# from resources import (
-#   UserRegistration,
-#   UserLogin,
-#   UserLogoutAccess,
-#   UserLogoutRefresh,
-#   TokenRefresh,
-#   TestSecretAccess
-# )
 from config import *
 from models import *
 
@@ -26,102 +27,178 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
 
-##### BELOW IS JWT RELATED #####
-
-@jwt.token_in_blacklist_loader
-def check_if_token_in_blacklist(decrypted_token):
-  jti = decrypted_token['jti']
-  return models.RevokedTokenModel.is_jti_blacklisted(jti)
-
-# api.add_resource(UserRegistration, '/JWTregistration')
-# api.add_resource(UserLogin, '/JWTlogin')
-# api.add_resource(UserLogoutAccess, '/JWTlogout/access')
-# api.add_resource(UserLogoutRefresh, '/JWTlogout/refresh')
-# api.add_resource(TokenRefresh, '/token/refresh')
-# api.add_resource(TestSecretAccess, '/secret')
-
-##### ABOVE IS JWT RELATED #####
-
-'''
-I tried to replace my simple user management with JWT for a more well 
-rounded solution. However I got stuck on python doesn't allow me to import 
-my resouces.py file (Hence the commented out lines above, code wouldn't run with it). 
-Still trying to figure out how to solve this problem, But due to time constraints, 
-I thought it would be more efficient if I can turn it in on Tuesday Night with 
-the current user management system.
-'''
-
+#===============Below is simple version of user management system==================
 # Register user into the system
 # Expected Input: accName (STR), displayName (STR), password (STR), password2 (STR)
-@app.route('/register/', methods = ['POST'])
-def register():
-  if request.json['password'] == request.json['password2']:
-    hashed_pw = generate_password_hash(request.json['password'], method='sha256')
-    new_user = UserLogin(
-      accName = request.json['accName'],
-      displayName = request.json['displayName'],
-      pwHash = hashed_pw
-    )
-    try:
-      db.session.add(new_user)
-      db.session.commit()
-    except exc.IntegrityError as e:
-      db.session.rollback()
-      return jsonify({ # Or return redirect(url_for('register'))
-        'Status': f'Failed',
-        'Message': f'User already exists, please tyr another accName',
-        'Error': str(e)
-      }), 400
-  else:
-    return jsonify({ # Or return redirect(url_for('register'))
-      'Status': f'Failed',
-      'Message': f'Please check password again.'
-    }), 400
+# @app.route('/registration/', methods = ['POST'])
+# def register():
+#   if request.json['password'] == request.json['password2']:
+#     hashed_pw = generate_password_hash(request.json['password'], method='sha256')
+#     new_user = UserLogin(
+#       accName = request.json['accName'],
+#       displayName = request.json['displayName'],
+#       pwHash = hashed_pw
+#     )
+#     try:
+#       db.session.add(new_user)
+#       db.session.commit()
+#     except exc.IntegrityError as e:
+#       db.session.rollback()
+#       return jsonify({ # Or return redirect(url_for('register'))
+#         'Status': f'Failed',
+#         'Message': f'User already exists, please tyr another accName',
+#         'Error': str(e)
+#       }), 400
+#   else:
+#     return jsonify({ # Or return redirect(url_for('register'))
+#       'Status': f'Failed',
+#       'Message': f'Please check password again.'
+#     }), 400
 
-  return jsonify({ # Or return redirect(url_for('login'))
-    'Status': f'Success',
-    'Message': f'User registered.'
-  }), 200
+#   return jsonify({ # Or return redirect(url_for('login'))
+#     'Status': f'Success',
+#     'Message': f'User registered.'
+#   }), 200
 
 # Logs current user into the system 
 # Expected Input: accName (STR), password (STR)
-@app.route('/login/', methods = ['POST'])
-def login():
-  if 'accName' in session:
-    return jsonify({ # Or return redirect(url_for('login'))
-      'Status': f'Failed',
-      'Message': f'Already Logged in, please log out first'
-    }), 400
-  user = UserLogin.query.filter_by(accName=request.json['accName']).first()
-  if user:
-    if check_password_hash(user.pwHash, request.json['password']):
-      session['userID'] = user.userID
-      session['accName'] = user.accName
-      return jsonify({ # Or return redirect(url_for('home'))
-        'Status': f'Success',
-        'Message': f'User logged in.'
-      }), 200
-    else:
-      return jsonify({ # Or return redirect(url_for('login'))
-        'Status': f'Failed',
-        'Message': f'Account or Password incorrect.'
-      }), 400
+# @app.route('/login/', methods = ['POST'])
+# def login():
+#   if 'accName' in session:
+#     return jsonify({ # Or return redirect(url_for('login'))
+#       'Status': f'Failed',
+#       'Message': f'Already Logged in, please log out first'
+#     }), 400
+#   user = UserLogin.query.filter_by(accName=request.json['accName']).first()
+#   if user:
+#     if check_password_hash(user.pwHash, request.json['password']):
+#       session['userID'] = user.userID
+#       session['accName'] = user.accName
+#       return jsonify({ # Or return redirect(url_for('home'))
+#         'Status': f'Success',
+#         'Message': f'User logged in.'
+#       }), 200
+#     else:
+#       return jsonify({ # Or return redirect(url_for('login'))
+#         'Status': f'Failed',
+#         'Message': f'Account or Password incorrect.'
+#       }), 400
 
 # Logs out current user
-@app.route('/logout/')
-def logout():
-  if 'accName' not in session:
-    return jsonify({ # Or return redirect(url_for('login'))
-      'Status': f'Failed',
-      'Message': f'Not logged in, cannot log out'
-    }), 400
+# @app.route('/logout/')
+# def logout():
+#   if 'accName' not in session:
+#     return jsonify({ # Or return redirect(url_for('login'))
+#       'Status': f'Failed',
+#       'Message': f'Not logged in, cannot log out'
+#     }), 400
 
-  session.pop('accName', None)
+#   session.pop('accName', None)
 
-  return jsonify({ # Or return redirect(url_for('home'))
-    'Status': f'Success',
-    'Message': f'User logged out'
-  }), 200
+#   return jsonify({ # Or return redirect(url_for('home'))
+#     'Status': f'Success',
+#     'Message': f'User logged out'
+#   }), 200
+#===============Above is simple version of user management system==================
+
+# Required for logout & refresh
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+  jti = decrypted_token['jti']
+  return RevokedTokenModel.is_jti_blacklisted(jti)
+
+#JWT Registration
+@app.route('/registration/', methods = ['POST'])
+def registration():
+  accName = request.json['accName']
+
+  if UserLogin.find_by_accName(accName):
+    return {'message': f'user name {accName} already exists'}
+  
+  new_user = UserLogin(
+    accName = accName,
+    displayName = request.json['displayName'],
+    pwHash = UserLogin.generate_hash(request.json['password'])
+  )
+
+  try:
+    # when save user to db, generate access and refresh token
+    new_user.save_to_db()
+    access_token = create_access_token(identity=accName)
+    refresh_token = create_refresh_token(identity=accName)
+    return {
+      'Message': f'User {accName} created',
+      'access_token': access_token,
+      'refresh_token': refresh_token
+    }
+  except:
+    db.session.rollback()
+    return {'Message': 'Something went wrong'}, 500
+
+# JWT login
+@app.route('/login/', methods = ['POST'])
+def login():
+  accName = request.json['accName']
+
+  # get user data by accName
+  cur_user = UserLogin.find_by_accName(accName)
+
+  if not cur_user:
+    return {'Message': f'User {accName} does not exist'}
+
+  if UserLogin.verify_hash(request.json['password'], cur_user.pwHash):
+    access_token = create_access_token(identity=accName)
+    refresh_token = create_refresh_token(identity=accName)
+
+    return {
+      'message': f'Logged in as {accName}',
+      'access_token': access_token,
+      'refresh_token': refresh_token
+      }
+  else:
+    return {'message': "Wrong credentials"}
+
+# JWT Logout access
+@app.route('/logout/access/', methods = ['POST'])
+@jwt_required
+def logoutAccess():
+  jti = get_raw_jwt()['jti']
+
+  try:
+    #revoking access token
+    revoked_token = RevokedTokenModel(jti=jti)
+    revoked_token.add()
+    return{'Message':f'Access token revoked'}
+  except:
+    return {'Message': 'Something went wrong'}, 500
+
+# JWT Logout refresh
+@app.route('/logout/refresh/', methods = ['POST'])
+@jwt_refresh_token_required
+def logoutRefresh():
+  jti = get_raw_jwt()['jti']
+
+  try:
+    revoked_token = RevokedTokenModel(jti=jti)
+    revoked_token.add()
+    pdb.set_trace()
+    return {'Message': 'Refresh token revoked'}
+  except:
+    return {'Message': 'Something went wrong'}, 500
+
+# JWT Token refresher
+@app.route('/token/refresh/', methods = ['POST'])
+@jwt_refresh_token_required
+def tokenRefresh():
+  cur_user = get_jwt_identity()
+  access_token = create_access_token(identity=cur_user)
+  return {'access_token': access_token}
+
+# JWT simple test for token validity
+@app.route('/test', methods = ['GET'])
+@jwt_required
+def TestSecretAccess(self):
+  return {'answer': 'Okay, it is working, jwt let\'s goooo'}
 
 #===================================================================================================
 # Add Comments to site
